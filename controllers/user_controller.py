76e12ae229a1dda284381aeb658e83fef857af3f
oleslav <oleslav.boychuk@gmail.com>
2021-05-25 07:29:03 +0300

    Updated static pages layout

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -6,21 +6,21 @@ from db_functionality import *
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('vitality/index.html')
 
 @app.route('/login', methods=['GET'])
 def login():
-    return render_template('login.html')
+    return render_template('vitality/login.html')
 
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    return render_template('login.html', error=False, message="You have been successfully logout")
+    return render_template('vitality/login.html', error=False, message="You have been successfully logout")
 
 
 @app.route('/signup', methods=['GET'])
 def signup():
-    return render_template('signup.html')
+    return render_template('vitality/signup.html')
 
 
 @app.route('/signup', methods=['POST'])
@@ -28,7 +28,7 @@ def signup_post():
     email = request.form.get('username')
 
     if not get_user_by_email(email) is None:
-        return render_template('signup.html', error=True, message="Account with this email already exists")
+        return render_template('vitality/signup.html', error=True, message="Account with this email already exists")
 
     password = request.form.get('password')
     username = email
@@ -40,7 +40,7 @@ def signup_post():
         db.session.commit()
         return redirect("/articles")  # get_all_articles()
     else:
-        return render_template('signup.html', error=True, message="Oops, fields are incorrect")
+        return render_template('vitality/signup.html', error=True, message="Oops, fields are incorrect")
 
 
 @app.route('/login', methods=['POST'])
@@ -49,14 +49,14 @@ def login_post():
     user = get_user_by_email(email)
 
     if user is None:
-        return render_template('login.html', error=True, message="Credential are wrong")
+        return render_template('vitality/login.html', error=True, message="Credential are wrong")
 
     password = request.form.get('password')
 
     if check_password_hash(user.password, password):
         return redirect("/articles")
     else:
-        return render_template('login.html', error=True, message="Credential are wrong")
+        return render_template('vitality/login.html', error=True, message="Credential are wrong")
 
 
 @app.route('/users', methods=['GET'])
